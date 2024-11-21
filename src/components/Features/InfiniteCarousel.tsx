91308904c6abaee5f564a2fd1ede18ab7b126cb7
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2024-11-21 15:25:51 +0530

    Update InfiniteCarousel

---
+++ src/components/Features/InfiniteCarousel.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Features/InfiniteCarousel.css"; // CSS for custom animation
+
+const InfiniteCarousel: React.FC = () => {
+  return (
+    <div className="slider relative w-full h-[250px] md:h-[100px] overflow-hidden bg-white shadow-lg">
+      {/* Left gradient */}
+      <div className="absolute inset-y-0 left-0 w-[200px] bg-gradient-to-r from-white to-transparent z-10 "></div>
+
+      {/* Right gradient */}
+      <div className="absolute  inset-y-0 right-0 w-[200px] bg-gradient-to-l from-white to-transparent z-10 "></div>
+
+      {/* Slider track */}
+      <div className="slide-track flex animate-scroll bg-yellow-400">
+        {Array(3)
+          .fill([
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
+          ])
+          .flat()
+          .map((src, index) => (
+            <div key={index} className="slide flex-shrink-0 w-[250px] h-[400px]">
+              <img src={src} alt={`Slide ${index + 1}`} className="w-full h-[100px] object-contain mt-20" />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteCarousel;
