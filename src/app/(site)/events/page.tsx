f650923be27cf9e1afc6e4ebfe7fca4fd9d13239
Sayan Rakshit <145536447+devsayanR@users.noreply.github.com>
2024-11-26 17:42:08 +0530

    feat/35-Added event cards

--- src/app/(site)/events/page.tsx
+++ src/app/(site)/events/page.tsx
@@ -2,6 +2,47 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import Contact from "@/components/Contact";
 import { Metadata } from "next";
 import EventBanner from "@/components/eventData/eventBanner";
+import Event from "@/components/eventData/eventCard";
+
+const events = [
+  {
+    imageUrl: "/images/event/event1.jpeg",
+    title: "BNB Hack 2024 Q4: The Ultimate Battle of Hacker Heroes",
+    date: "Nov 30, 2024",
+    location: "Virtual",
+    tags: ["blockchain", "AI", "TG", "Hack", "DeFi"],
+    eventStatus: "Ongoing",
+    daysLeft: 30,
+  },
+  {
+    imageUrl: "/images/event/event1.jpeg",
+    title: "BNB Hack 2024 Q4: The Ultimate Battle of Hacker Heroes",
+    date: "Nov 30, 2024",
+    location: "Virtual",
+    tags: ["blockchain", "AI", "TG", "Hack", "DeFi"],
+    eventStatus: "Ongoing",
+    daysLeft: 30,
+  },
+  {
+    imageUrl: "/images/event/event1.jpeg",
+    title: "BNB Hack 2024 Q4: The Ultimate Battle of Hacker Heroes",
+    date: "Nov 30, 2024",
+    location: "Virtual",
+    tags: ["blockchain", "AI", "TG", "Hack", "DeFi"],
+    eventStatus: "Ongoing",
+    daysLeft: 30,
+  },
+  {
+    imageUrl: "/images/event/event1.jpeg",
+    title: "BNB Hack 2024 Q4: The Ultimate Battle of Hacker Heroes",
+    date: "Nov 30, 2024",
+    location: "Virtual",
+    tags: ["blockchain", "AI", "TG", "Hack", "DeFi"],
+    eventStatus: "Ongoing",
+    daysLeft: 30,
+  },
+];
+
 
 export const metadata: Metadata = {
   title:
@@ -11,9 +52,29 @@ export const metadata: Metadata = {
 
 const ContactPage = () => {
   return (
-<>
-<Breadcrumb pageName="Event Page" />
-<EventBanner/>
+    <>
+      <Breadcrumb pageName="Event Page" />
+      <div className="w-full">
+        <EventBanner/>
+        <h2 className="px-20 pt-5 font-bold sm:text-xl">
+            Explore Events
+            <span className="font-normal pl-1 text-slate-500">({events.length})</span>
+        </h2>
+        <div className="lg:px-20 py-6 sm:p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {events.map((event, index) => (
+            <Event
+              key={index}
+              imageUrl={event.imageUrl}
+              title={event.title}
+              date={event.date}
+              location={event.location}
+              tags={event.tags}
+              eventStatus={event.eventStatus}
+              daysLeft={event.daysLeft}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/eventData/eventCard.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { GoTag } from "react-icons/go";
+import { CiLocationOn } from "react-icons/ci";
+import { FaCalendarAlt } from "react-icons/fa";
+
+
+interface EventProps {
+  imageUrl: string;
+  title: string;
+  date: string;
+  location: string;
+  tags: string[];
+  eventStatus: string;
+  daysLeft: number;
+}
+
+const Event: React.FC<EventProps> = ({
+  imageUrl,
+  title,
+  date,
+  location,
+  tags,
+  eventStatus,
+  daysLeft,
+}) => {
+  return (
+    <div className="py-2 max-w-sm mx-auto bg-white dark:bg-dark-2 rounded-lg shadow-md border border-gray-700 dark:border-gray-300 group">
+      <div className="flex items-center justify-between pb-2 border-b border-gray-300 dark:border-gray-700">
+        <div className="flex items-center space-x-2 ml-auto pr-3">
+          <span className="text-xs font-semibold text-green-600 bg-green-100 dark:bg-green-800 dark:text-green-300 px-2 py-1 rounded">
+            {eventStatus}
+          </span>
+          <span className="text-xs text-orange-500">
+            {daysLeft} days left
+          </span>
+        </div>
+      </div>
+
+      <div className="p-3 relative w-full h-48 sm:h-40 md:h-52 lg:h-48 overflow-hidden group">
+        <Image
+          src={imageUrl}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="transition-transform duration-500 ease-out group-hover:scale-110"
+          priority
+        />
+      </div>
+
+      <div className="px-4 py-2">
+        <h2 className="text-lg font-bold text-gray-800 dark:text-white">
+          {title}
+        </h2>
+        <div className="flex items-center justify-between">
+          <p className="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400 mt-1">
+            <CiLocationOn className="text-lg text-black dark:text-white font-bold" />{location}
+          </p>
+          <p className="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400 mt-1">
+            <FaCalendarAlt />{date}
+          </p>
+        </div>
+        <div className="flex items-center flex-wrap gap-2 mt-4">
+          <GoTag className="font-bold" />
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="text-xs font-medium bg-blue-200 text-blue-500 px-2 py-1 rounded"
+            >{tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
