9adcbeaaab2f43320595b44c1879da2bc1e73be5
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2024-11-28 10:46:01 +0530

    feat/47 : added-relevant-images (#48)
    * Add files via upload
    
    * Added-Relevent-images

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -62,14 +62,14 @@ const Header = () => {
                 {pathUrl !== "/" ? (
                   <>
                     <Image
-                      src={`/images/logo/logo.svg`}
+                      src={`/images/logo/logo.png`}
                       alt="logo"
                       width={240}
                       height={30}
                       className="header-logo w-full dark:hidden"
                     />
                     <Image
-                      src={`/images/logo/logo-white.svg`}
+                      src={`/images/logo/logo.png`}
                       alt="logo"
                       width={240}
                       height={30}
@@ -80,8 +80,8 @@ const Header = () => {
                   <>
                     <Image
                       src={`${sticky
-                        ? "/images/logo/logo.svg"
-                        : "/images/logo/logo-white.svg"
+                        ? "/images/logo/logo.png"
+                        : "/images/logo/logo.png"
                         }`}
                       alt="logo"
                       width={140}
@@ -89,7 +89,7 @@ const Header = () => {
                       className="header-logo w-full dark:hidden"
                     />
                     <Image
-                      src={"/images/logo/logo-white.svg"}
+                      src={"/images/logo/logo.png"}
                       alt="logo"
                       width={140}
                       height={30}
