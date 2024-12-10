8be6084427dae857a07550261e6163bb8b4db5d9
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2024-12-10 23:03:54 +0530

    Updating_FeatureSection_For_Dark_Mode_Change_the_Logo

--- src/components/Features/InfiniteCarousel.tsx
+++ src/components/Features/InfiniteCarousel.tsx
@@ -3,7 +3,7 @@ import "../Features/InfiniteCarousel.css"; // CSS for custom animation
 
 const InfiniteCarousel: React.FC = () => {
   return (
-    <div className="slider relative w-full h-[150px] md:h-[150px] overflow-hidden bg-white">
+    <div className="slider relative w-full h-[150px] md:h-[150px] overflow-hidden bg-white dark:bg-[#111928]">
       {/* Left gradient */}
       <div className="absolute inset-y-0 left-0 w-[200px] bg-gradient-to-r z-10"></div>
 
@@ -11,20 +11,21 @@ const InfiniteCarousel: React.FC = () => {
       <div className="absolute inset-y-0 right-0 w-[200px] bg-gradient-to-l z-10"></div>
 
       {/* Slider track */}
-      <div className="slide-track flex animate-scroll bg-yellow-400">
+      <div className="slide-track flex animate-scroll">
         {Array(3)
           .fill([
-            "https://img.icons8.com/?size=96&id=fpGM2cINbbu4&format=png",
+            
             "https://imgix.datadoghq.com/img/dd_logo_n_70x75.png?ch=Width,DPR&fit=max&auto=format&w=70&h=75&dpr=2",
-            "https://www.ibm.com/brand/experience-guides/developer/57dcf7c808bca7c58a7b6eabb80617c9/ibm-8-bar-positive.svg",
-            "https://img.icons8.com/?size=160&id=OPUdQmdeIEMR&format=png",
-            "https://img.icons8.com/?size=100&id=12599&format=png",
+            "https://img.icons8.com/?size=100&id=24662&format=png&color=228BE6",
+            "https://img.icons8.com/?size=96&id=fpGM2cINbbu4&format=png",
+            "https://img.icons8.com/?size=100&id=12599&format=png&color=228BE6",
             "https://img.icons8.com/?size=160&id=RduYmqw5H7xm&format=png",
+            "https://img.icons8.com/?size=100&id=uVERmCBZZACL&format=png&color=228BE6",
           ])
           .flat()
           .map((src, index) => (
-            <div key={index} className="slide flex-shrink-0 w-[250px] h-[150px] flex items-center justify-center">
-              <img src={src} alt={`Slide ${index + 1}`} className="w-full h-[100px] object-contain" />
+            <div key={index} className="slide flex-shrink-0 w-[200px] h-[150px] flex items-center justify-center">
+              <img src={src} alt={`Slide ${index + 1}`} className="w-full h-[90px] object-contain" />
             </div>
           ))}
       </div>
