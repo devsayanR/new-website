51613c5e5267c35c840c6b0b96a064a9aa0c6d58
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2024-12-16 10:06:37 +0530

    Resolve header logo for both light and dark mode (#60)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -48,16 +48,16 @@ const Header = () => {
     <>
       <header
         className={`ud-header left-0 top-0 z-40 flex w-full items-center ${sticky
-          ? "shadow-nav fixed z-[999] border-b border-stroke bg-white/80 backdrop-blur-[5px] dark:border-dark-3/20 dark:bg-dark/10"
+          ? "shadow-nav fixed z-[999] border-b border-none bg-white/30 backdrop-blur-md dark:border-dark-3/20 dark:bg-dark/10 h-[100px]"
           : "absolute bg-transparent"
           }`}
       >
         <div className="container">
-          <div className="relative -mx-4 flex items-center justify-between">
+          <div className="relative -mx-4  flex items-center justify-between">
             <div className="w-60 max-w-full px-4">
               <Link
                 href="/"
-                className={`logo navbar-logo block w-full ${sticky ? "py-2" : "py-5"
+                className={`logo navbar-logo block w-full ${sticky ? "py-2" : "py-9"
                   } `}
               >
                 {pathUrl !== "/" ? (
@@ -81,8 +81,8 @@ const Header = () => {
                   <>
                     <Image
                       src={`${sticky
-                        ? "/images/logo/logo.svg"
-                        : "/images/logo/logo-white.svg"
+                        ? "/images/logo/logo_mob.svg"
+                        : "/images/logo/logo_new.svg"
                         }`}
                       alt="logo"
                       width={140}
@@ -90,7 +90,7 @@ const Header = () => {
                       className="header-logo w-full dark:hidden"
                     />
                     <Image
-                      src={"/images/logo/logo-white.svg"}
+                      src={"/images/logo/logo_new.svg"}
                       alt="logo"
                       width={140}
                       height={30}
