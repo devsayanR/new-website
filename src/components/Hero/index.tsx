70a090f3060a0c7098e3ce77a216915477601ff9
Rishi Mondal <mavrickrishi@gmail.com>
2024-12-16 15:01:56 +0530

    fix(hero):Update Hero component title and description to reflect new focus on AI and open source solutions (#65)

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -16,13 +16,10 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
-                  Open-source SaaS Starter Kit and Boilerplate for Next.js
+                  DevRhylme Foundation: Innovating AI and Open Source Solutions
                 </h1>
                 <p className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
-                  Next.js SaaS Boilerplate and Starter Kit designed and built
-                  for SaaS startups. It comes with all necessary integrations,
-                  pages, and components you need to launch a feature-rich SaaS
-                  websites.
+                  Advancing Blockchain, Go, and Python Libraries with Cutting-Edge AI. Building a Future for Open Source Communities Everywhere.
                 </p>
                 <ul className="mb-10 flex flex-wrap items-center justify-center gap-5">
                   <li>
