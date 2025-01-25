1ecc0e6529dd0ce2f1fe2daaa57ab85c699802fb
Sayan Rakshit <145536447+devsayanR@users.noreply.github.com>
2025-01-25 21:42:08 +0530

    feat(project-card): refactor github api integration and add buttons on project card component (#113)

---
+++ src/components/projectData/Card.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Repo } from './'
+import Link from 'next/link'
+import { FaGithub, FaLink } from "react-icons/fa";
+import { IoBookSharp } from "react-icons/io5";
+
+
+const Card = ({ repo }: { repo: Repo }) => {
+  return (
+    <div key={repo.id}
+      className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 dark:bg-gray-800 dark:border-gray-700 relative"
+    >
+      <h2 className="text-xl font-bold mb-2 dark:text-gray-400 text-gray-900">
+        {repo.Name}
+      </h2>
+      <p className="text-gray-700 dark:text-[rgb(136,143,155)]">
+        {repo.Description || "No description available."}
+      </p>
+      <div className="flex justify-start items-center mt-4 mb-24 sm:mb-12">
+        <span className="px-3 py-1 text-xs text-white bg-gray-600 rounded-full shadow-md float-right">
+          <span className='font-semibold'>{repo.Language_used || "N/A"}</span>
+        </span>
+      </div>
+      <div className='flex items-center gap-4 flex-wrap text-white absolute bottom-5'>
+        <Link href={repo.Repo_link} target='_blank' className='flex items-center justify-center gap-1 px-2 py-1 bg-gray-600 rounded-md hover:bg-gray-700 transition'>
+          <FaGithub />
+          Repository
+        </Link>
+        <Link href={repo.Docs_link} target="_blank" className='flex items-center justify-center gap-1 px-2 py-1 bg-blue-600 rounded-md hover:bg-blue-700 transition'>
+          <IoBookSharp />
+          Docs
+        </Link>
+        {
+          repo.Website_link && (
+            <Link href={repo.Website_link} target='_blank' className='flex items-center justify-center gap-1 px-2 py-1 bg-green-600 rounded-md hover:bg-green-700 transition'>
+              <FaLink />
+              Website
+            </Link>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/projectData/index.tsx
+++ src/components/projectData/index.tsx
@@ -2,16 +2,18 @@
 import { useEffect, useState } from "react";
 import "@/components/projectData/projects.css";
 import PreLoader from "../Common/PreLoader";
+import Card from "./Card";
+
 
 // Define the repository data structure
-interface Repo {
+export interface Repo {
   id: number;
-  html_url: string;
-  name: string;
-  description: string | null;
-  language: string | null;
-  stargazers_count: number;
-  forks_count: number; // Add forks_count
+  Name: string;
+  Description: string | null;
+  Language_used: string | null;
+  Repo_link: string | "";
+  Docs_link: string | "";
+  Website_link: string | "";
 }
 
 const RepoList = () => {
@@ -25,6 +27,7 @@ const RepoList = () => {
         const response = await fetch("/api/github"); // Ensure endpoint is correct
         if (!response.ok) throw new Error("Network response was not ok");
         const data: Repo[] = await response.json(); // Cast the response data to Repo[]
+        console.log(data)
         setRepos(data);
       } catch (err) {
         if (err instanceof Error) {
@@ -52,32 +55,9 @@ const RepoList = () => {
   if (!repos.length) return <div>No repositories found.</div>;
 
   return (
-    <div className="projectsCard grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    <div className="projectsCard grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 ">
       {repos.map((repo) => (
-        <a
-        href={repo.html_url}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={repo.id}
-          className="p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 dark:bg-gray-800 dark:border-gray-700"
-      >
-          <h2 className="text-xl font-bold mb-2 dark:text-gray-400 text-gray-900">
-            
-              {repo.name}
-            
-          </h2>
-          <p className="text-gray-700 dark:text-[rgb(136,143,155)]">
-            {repo.description || "No description available."}
-          </p>
-          <div className="flex justify-between items-center mt-4">
-            <span className="text-sm text-gray-600 dark:text-[rgb(136,143,155)]">
-              Language: {repo.language || "N/A"}
-            </span>
-            <span className="text-sm text-gray-600 dark:text-[rgb(136,143,155)]">
-              ⭐ {repo.stargazers_count}
-            </span>
-          </div>
-    </a>
+        <Card repo={repo} />
       ))}
     </div>
   );
